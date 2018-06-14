44547922b801b926514f80819aed3d7f7a8361f1
Yuriy Stetskyi <yst@interlogic.com.ua>
2018-06-14 12:12:09 +0300

    Created Wiki_Dapper.DataAccess project

---
+++ Wiki_Dapper/Wiki_Dapper.DataAccess/Interfaces/ICommentRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wiki_Dapper.Entities.Models;
+
+namespace Wiki_Dapper.DataAccess.Interfaces
+{
+    public interface ICommentRepository : IRepository<Comment>
+    {
+    }
+}
